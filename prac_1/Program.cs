aab64e18a5011ca2c80b6f582ef5f57db6ec7a31
aidyn <aidyn@MAZATRON>
2023-10-20 19:55:56 +0600

    Добавьте файлы проекта.

---
+++ prac_1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//1.
+namespace prac_1
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1");
+            // Задание 1
+            GreetUser();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Задание 2");
+            // Задание 2
+            SumNumbers();
+        }
+
+        static void GreetUser()
+        {
+            Console.Write("Введите ваше ФИО:  ");
+            string fio = Console.ReadLine();
+            string greeting = "Приветствую тебя - " + fio;
+            Console.WriteLine(greeting);
+        }
+
+        static void SumNumbers()
+        {
+            Console.Write("Введите несколько чисел через пробел:  ");
+            string input = Console.ReadLine();
+
+            string[] numberStrings = input.Split(' ');
+            int sum = 0;
+
+            for (int i = 0; i < numberStrings.Length; i++)
+            {
+                if (int.TryParse(numberStrings[i], out int number))
+                {
+                    sum += number;
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка: '{numberStrings[i]}' не является числом и будет проигнорировано.");
+                }
+            }
+
+            Console.WriteLine($"Сумма введенных чисел: {sum}");
+        }
+    }
+
+}
